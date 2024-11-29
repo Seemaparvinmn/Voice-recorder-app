b1f31624a3d6c1a47f2686ec3868087ae5223de3
seemaparvinmn <145323108+Seemaparvinmn@users.noreply.github.com>
2024-11-30 00:48:43 +0530

    Update voice_record_python.py

--- voice_record_python.py
+++ voice_record_python.py
@@ -1,28 +1,17 @@
-#
-
-
-# import sounddevice
-# from scipy.io.wavfile import write
-
-
-# def sharadvoice_recorder(seconds,file):
-#     print("Your Recording Started.....")
-#     sharadrecording=sounddevice.rec((seconds*44100),samplerate=44100,channels=2)
-#     sounddevice.wait()
-#     write(file,44100,sharadrecording)
-#     print(" Recordin is finished")
-
-
-# sharadvoice_recorder(10," Recordsave.wav")
 
 
 
+#import sounddevice as sd
 import sounddevice as sd
 from scipy.io.wavfile import write
+#from scipy.io.wavfile import write
+
+
 
 def seema_recorder(seconds,file):
     print("Your recording Started.......")
-    seemarecording=sd.rec(int(seconds*44100),samplerate=44100,channels=1)
+    seemarecording=sd.rec(int(seconds*44100),samplerate=44100,channels=1)#4100 is the sampling rate, meaning there are 44100 samples per second.
+# Multiplying it by seconds gives the total number of frames for the recording duration
     sd.wait()
     write(file,44100,seemarecording)
     print("Recording is finished")
